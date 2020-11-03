4627da505ecb709bfb05854b8321305d30d0e497
thz-hub <62689630+thz-hub@users.noreply.github.com>
2020-11-03 14:49:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,19 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-mkdir -p package/custom
-cd package/custom
-git clone https://github.com/openwrt-develop/luci-theme-atmaterial
-git clone https://github.com/tty228/luci-app-serverchan
-git clone https://github.com/rufengsuixing/luci-app-adguardhome
-git clone -b lede https://github.com/pymumu/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns
-git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
-git clone https://github.com/vernesong/OpenClash
-git clone https://github.com/Lienol/openwrt-package
-git clone https://github.com/fw876/helloworld
-git clone https://github.com/kenzok8/openwrt-packages
-git clone https://github.com/kenzok8/small
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
