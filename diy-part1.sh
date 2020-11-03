97d166ab644495085dc075ed92142627d669d7b9
thz-hub <62689630+thz-hub@users.noreply.github.com>
2020-11-03 15:58:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,7 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+mkdir -p package/custom
+cd package/custom
+git clone https://github.com/tty228/luci-app-serverchan
