26c4649a6d57c7cd023b970b471d548bdc0fbd08
thz-hub <62689630+thz-hub@users.noreply.github.com>
2020-11-03 14:52:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git tty228 hhttps://github.com/tty228' feeds.conf.default
